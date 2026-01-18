7467c4baa25953595477f07a2a6fdf6e38760cfa
manishverma369 <indiancinemajagat@gmail.com>
2026-01-18 14:51:01 +0000

    fron energy

---
+++ jump frog eergy.cpp
@@ -0,0 +1,7 @@
+// ek frog hai jo ki 0 pr hai abhi but usse top yani ki n tak jana hai with using minimum energy 
+// jumping style 1 step ya 2 step;
+// min energy=h[i]-h[j];
+// stairs ki apni height hai 
+int jump(vector<int>st,int n){
+    
+}
